11f39135b1bf74900e1293b191c1952996e76da9
sowmik-sec <sowmik.sec@gmail.com>
2023-06-07 13:08:10 +0600

    create home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,24 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Hero from "public/hero.png";
+// import Button from "@/components/Button/Button";
+
 export default function Home() {
   return (
-    <div>
-      <Image
-        src="https://images.pexels.com/photos/17041911/pexels-photo-17041911.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-        alt=""
-        width={500}
-        height={500}
-        className={styles.img}
-      />
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your Idea into Reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        {/* <Button url="/portfolio" text="See Our Works" /> */}
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="" className={styles.img} />
+      </div>
     </div>
   );
 }
