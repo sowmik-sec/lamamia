78633427e166b9a783d5f352e57348b1782cb9ea
sowmik-sec <sowmik.sec@gmail.com>
2023-06-07 13:00:46 +0600

    use Image in next js

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,16 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-
+import Hero from "public/hero.png";
 export default function Home() {
-  return <div>hello world</div>;
+  return (
+    <div>
+      <Image
+        src="https://images.pexels.com/photos/17041911/pexels-photo-17041911.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+        width={500}
+        height={500}
+        className={styles.img}
+      />
+    </div>
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,40 @@
 import React from "react";
 import styles from "./page.module.css";
+import Image from "next/image";
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>©2023 Habib. All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          src="/1.png"
+          alt="lamadev fb"
+          className={styles.icon}
+          width={15}
+          height={15}
+        />
+        <Image
+          src="/2.png"
+          alt="lamadev insta"
+          className={styles.icon}
+          width={15}
+          height={15}
+        />
+        <Image
+          src="/3.png"
+          alt="lamadev twitter"
+          className={styles.icon}
+          width={15}
+          height={15}
+        />
+        <Image
+          src="/4.png"
+          alt="lamadev youtube"
+          className={styles.icon}
+          width={15}
+          height={15}
+        />
+      </div>
     </div>
   );
 };
